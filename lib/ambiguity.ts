720d7c00ef1cabd0863aad63895a76189427300b
Aram Panasenco <panasenco@users.noreply.github.com>
2022-10-04 17:34:59 -0600

    First graphology use

--- lib/ambiguity.ts
+++ lib/ambiguity.ts
@@ -1,3 +1,20 @@
-var greet : string = "Welcome to";
-var orgName : string = "GeeksforGeeks!";
-console.log(greet+ " "+orgName);
+import Graph from 'graphology';
+
+const graph : Graph = new Graph();
+
+// Adding some nodes
+graph.addNode('John');
+graph.addNode('Martha');
+
+// Adding an edge
+graph.addEdge('John', 'Martha');
+
+// Displaying useful information about your graph
+console.log('Number of nodes', graph.order);
+console.log('Number of edges', graph.size);
+
+// Iterating over nodes
+graph.forEachNode(node => {
+  console.log(node);
+});
+
